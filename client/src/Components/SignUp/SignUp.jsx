27e10baf4bfe8779ebdf3b4aabb1a3fd602cc6f3
miteshp2110 <paliwalmitesh2110@gmail.com>
2023-10-14 21:35:58 +0530

    resolved

--- client/src/Components/SignUp/SignUp.jsx
+++ client/src/Components/SignUp/SignUp.jsx
@@ -5,6 +5,8 @@ import NavBar from '../NavBar/NavBar';
 const SignUp=()=>{
 
     return(
+      <>
+      <NavBar/>
         
 <section className="background-radial-gradient overflow-hidden">
   <div className="container px-4 py-5 px-md-5 text-center text-lg-start my-5">
@@ -76,6 +78,7 @@ const SignUp=()=>{
     </div>
   </div>
 </section>
+</>
 
     )
 
