f6098cca6813337ff9792a9016e586f48ab66fde
Hrishabh Gupta <113886587+Hrishabh3829@users.noreply.github.com>
2023-10-16 16:03:21 +0530

    Create animation.jsx

---
+++ client/src/Components/LearnFirst/animation.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './background-animation.css'; 
+
+const MyComponent = () => {
+  return (
+    <div className="animated-background">
+     
+    </div>
+  );
+};
+
+export default MyComponent;
