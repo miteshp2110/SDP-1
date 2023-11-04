d796dc3ab02ecbbf6d5b60b34c58393f5ad41d3f
Gaurav-Khivasara <gkgauravgk123@gmail.com>
2023-11-04 21:39:19 +0530

    Dashboard progress

--- client/src/Components/Dashboard/DashBoard.jsx
+++ client/src/Components/Dashboard/DashBoard.jsx
@@ -1,46 +1,62 @@
 import './DashboardStyle.css'
 
+const space = ' ';
 const Dashboard=()=>{
     return(
         <div className="dashboard-container">
-            <div style={{textAlign: "center", fontSize: 50}}>This is a Dashboard</div>
-            <br />
+            <div className="title">
+                Dashboard
+                <hr style={{backgroundColor: "lightgray", height: 1, border: "none"}} />
+            </div>
             <div className="dashboard-contents">
                 <div className="funds">
-                    Funds<br /><br />Avl. balance<br />1234.55
+                    <div className="funds-subtitle">
+                        Funds
+                    </div>
+                    <span className="avl-bal-name">Avl. balance:&nbsp;</span>
+                    <span id="avl-balance">
+                        1234.55
+                    </span>
+                    <br />
+                    <button className="btn" id="add-btn">+ Add</button>
+                    <button className="btn" id="withdraw-btn"><img src=""/>Withdraw</button>
                 </div>
-                <button id="add-btn">+ Add</button>
-                <button id="withdraw-btn"><img src=""/>Withdraw</button>
-                <table className="positions">
-                    <caption style={{textAlign: "left", marginLeft: 10}}>Positions</caption>
-                    <tr>
-                        <td>Hii</td>
+                <table className="tables" id="positions" border={1} cellSpacing={0} cellPadding={1}>
+                    <caption style={{textAlign: "left", fontSize: 25}}>Positions</caption>
+                    <tr style={{fontWeight: "bold"}}>
+                        <td>Instrument</td>
+                        <td>Type</td>
+                        <td>Qty.</td>
+                        <td>Avg. cost</td>
+                        <td>LTP</td>
+                        <td>Change</td>
                     </tr>
                 </table>
-                <table className="holdings" border={1} cellSpacing={0} cellPadding={1}>
-                    <caption style={{textAlign: "left", marginLeft: 10}}>Holdings</caption>
+                <table className="tables" id="holdings" border={1} cellSpacing={0} cellPadding={1}>
+                    <caption style={{textAlign: "left", fontSize: 25}}>Holdings</caption>
                     <tr style={{fontWeight: "bold"}}>
                         <td>Instrument</td>
                         <td>Qty.</td>
                         <td>Avg. cost</td>
-                        <td>Invested amt.</td>
+                        <td>LTP</td>
+                        <td>Change</td>
                     </tr>
                     <tr>
                         <td>Instrument 1</td>
                         <td>Qty</td>
                         <td>Avg. price</td>
-                        <td>Total amt.</td>
+                        <td>Last Trade Price</td>
+                        <td>%chng</td>
                     </tr>
                     <tr>
                         <td>Instrument 2</td>
                         <td>Qty</td>
                         <td>Avg. price</td>
-                        <td>Total amt.</td>
+                        <td>Last Trade Price</td>
+                        <td>%chng</td>
                     </tr>
                 </table>
             </div>
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
-            <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             <br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
             Hii
         </div>
