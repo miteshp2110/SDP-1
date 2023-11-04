8aff2ed61e4d4d4533acb2dad28c39959f120a5a
miteshp2110 <paliwalmitesh2110@gmail.com>
2023-11-04 13:14:58 +0530

    littlechanges

--- client/src/Components/Home/Home.jsx
+++ client/src/Components/Home/Home.jsx
@@ -1,6 +1,15 @@
+import './HomeStyle.css'
 const Home=()=>{
     return(
-        <div>This is a Home</div>
+        <>
+        <div className='home_username'>
+            WELCOME USER.
+
+        </div>
+            <div className="Journey_card">
+                sad
+            </div>
+        </>
     )
 }
 export default Home;
